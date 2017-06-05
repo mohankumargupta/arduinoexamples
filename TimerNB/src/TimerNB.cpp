493b6ad4753470eb425f2a6d8231f3c98b6ad949
mohankumargupta <mohankumargupta@users.noreply.github.com>
2017-06-05 11:59:31 +1000

    Create TimerNB.cpp

---
+++ TimerNB/src/TimerNB.cpp
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/************************************************************
+    Call with:
+      IntervalMicros  The IntervalMicros elapsed time in uS
+       NextMicro  Pointer to an unsigned long, which is the caller's private time marker,
+
+    Return true if IntervalMicros uS have passed, alter NextMicro to the new point in time.
+*/
+
+int TestElapsedMicros_NB(unsigned long IntervalMicros, unsigned long *NextMicro)
+{
+  if (micros() - *NextMicro >= IntervalMicros) {
+    *NextMicro = *NextMicro + IntervalMicros;
+    return true ;
+  }
+  else {
+    return false;
+  }
+}
